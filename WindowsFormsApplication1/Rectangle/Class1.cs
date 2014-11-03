99d322cd1d048d2db9d5f99e4564ac1e24d8d1bc
Dmitry <bike.off@mail.ru>
2014-11-03 12:59:11 +0200

    crude
    the crude project - the first attempt to make something

---
+++ WindowsFormsApplication1/Rectangle/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangles
+{
+    public class Rectangle
+    {
+        public Rectangle(int leftCoord, int upCoord, int rightCoord, int downCoord)
+        {
+            up = upCoord;
+            down = downCoord;
+            left = leftCoord;
+            right = rightCoord;
+        }
+        public int left
+        {
+            get
+            {
+                return coordinateOfLeftBound;
+            }
+            set
+            {
+                coordinateOfLeftBound = value;
+            }
+        }
+        public int right
+        {
+            get
+            {
+                return coordinateOfRightBound;
+            }
+            set
+            {
+                coordinateOfRightBound = value;
+            }
+        }
+        public int up
+        {
+            get
+            {
+                return coordinateOfUpperBound;
+            }
+            set
+            {
+                coordinateOfUpperBound = value;
+            }
+        }
+        public int down
+        {
+            get
+            {
+                return coordinateOfLowerBound;
+            }
+            set
+            {
+                coordinateOfLowerBound = value;
+            }
+        }
+
+        private int coordinateOfUpperBound;
+        private int coordinateOfLowerBound;
+        private int coordinateOfLeftBound;
+        private int coordinateOfRightBound;
+    }
+}
---
+++ WindowsFormsApplication1/Square/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangles;
+
+namespace Squares
+{
+    public class Square : Rectangle
+    {
+        public Square(int leftCoord, int upCoord, int rightCoord, int downCoord)
+        {
+            up = upCoord;
+            down = downCoord;
+            left = leftCoord;
+            right = rightCoord;
+            correctCoordinatesForSquare();
+        }
+
+        private void correctCoordinatesForSquare() //wrong function - engaged later
+        {
+            int width, height, squareSideLength;
+
+            width = right - left;
+            height = down - up;
+            if (width > height)
+                squareSideLength = width;
+            else
+                squareSideLength = height;
+            right = left + squareSideLength;
+            down = up + squareSideLength;
+        }
+    }
+}
